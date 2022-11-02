fa6b82d74d6e1e39040954fd24f2b97016749e84
Mirko <112374229+MirkoVbl@users.noreply.github.com>
2022-11-02 17:10:11 +0100

    Add useState + useEffect

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,21 +1,38 @@
 import {useNavigate} from "react-router-dom";
+import {getRandomQuestion} from "../services/BackendService";
+import {useEffect, useState} from "react";
 
-export default function GamePage() {
+type GamePageProps = {
 
+}
+
+
+export default function GamePage(props: GamePageProps) {
+
+    const [question, setQuestion] = useState("")
+
+    useEffect(() => {
+        getRandomQuestion()
+            .then((q)=>setQuestion(q.questionText))
+
+
+    }, []);
 
     const navigate = useNavigate();
     const navToGame = () => {
-        navigate("/game")
+        navigate("/")
     }
 
     const submitNavigate = () => {
         navToGame();
     }
 
+
     return(
         <>
+            <p>{question}</p>
 
-
+            <button onClick={submitNavigate}>Aufhören</button>
         </>
     )
 }
